89d4ec547334526d8b01d7765bb03857c6919601
Vignesh <70033044+VigneshNk@users.noreply.github.com>
2021-01-24 00:38:36 -0500

    Add files via upload

--- Pathfinding/BFS.java
+++ Pathfinding/BFS.java
@@ -1,97 +1,93 @@
-package MazeProject;
+package Pathfinding;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class BFS 
-{
+import Visualization.Point;
+
+public class BFS {
 	private static Queue<Point> points = new LinkedList<Point>();
 
-	//Check if Coordinate is Valid in Maze
-	private static boolean ValidSquare(int[][] maze, int x, int y) 
-	{
-		
-		if(x >= 1 && x < 34 && y >= 1 && y < 24 && (maze[y][x] == 0 || maze[y][x] == 9)) 
-		{
+	// Check if Coordinate is Valid in Maze
+	private static boolean ValidSquare(int[][] maze, int x, int y) {
+		if (y >= 1 && y < maze.length - 1 && x >= 1 && x < maze[y].length - 1 && (maze[y][x] == 0 || maze[y][x] == 9)) {
 			return true;
 		}
+
 		return false;
 	}
 
-	//Check if Point is Already in Queue
-	private static boolean NonDuplicatePoint(int x, int y) 
-	{
-		for(Point temp : points) 
-		{
-			if(temp.getX() == x && temp.getY() == y) 
-			{
-				//Duplicate Point
+	// Check if Point is Already in Queue
+	private static boolean NonDuplicatePoint(int x, int y) {
+		for (Point temp : points) {
+			if (temp.getX() == x && temp.getY() == y) {
+				// Duplicate Point
 				return false;
 			}
 		}
 
-		//Point is Unique
+		// Point is Unique
 		return true;
 	}
 
-	private static Point FindPath(int[][] maze, int x, int y) 
-	{
-		//Add Initial Start Position to Queue
+	private static Point FindPath(int[][] maze, int x, int y, List<Integer> searchArea) {
+		// Add Initial Start Position to Queue
 		points.add(new Point(x, y, null));
 
-		while(!points.isEmpty()) 
-		{
+		// Adjacent Square Offsets
+		int[] dx = { 1, 0, -1, 0 };
+		int[] dy = { 0, 1, 0, -1 };
+
+		while (!points.isEmpty()) {
 			Point p = points.remove();
 
-			//Base Case
-			if(maze[p.y][p.x] == 9) 
-			{
-				//Destination Node Found
+			// Base Case
+			if (maze[p.getY()][p.getX()] == 9) {
+				// Destination Node Found
 				return p;
 			}
 
-			//Check Adjacent Squares
-			int[] dx = { 1, 0, -1, 0 };
-			int[] dy = { 0, 1, 0, -1 };
+			// Position Has Been Checked
+			maze[p.getY()][p.getX()] = 3;
 
-			for(int i = 0; i < 4; i++) 
-			{
-				//Check Neighbors
-				if(ValidSquare(maze, p.x + dx[i], p.y + dy[i]) && NonDuplicatePoint(p.x + dx[i], p.y + dy[i])) 
-				{
-					//Position Has Been Checked
-					maze[p.y][p.x] = 3;
+			// Add Square to Search Area
+			searchArea.add(p.getX());
+			searchArea.add(p.getY());
 
-					points.add(new Point(p.x + dx[i], p.y + dy[i], p));
+			for (int i = 0; i < 4; i++) {
+				// Check Neighbors
+				if (ValidSquare(maze, p.getX() + dx[i], p.getY() + dy[i])
+						&& NonDuplicatePoint(p.getX() + dx[i], p.getY() + dy[i])) {
+
+					// Add New Point to Search Queue
+					points.add(new Point(p.getX() + dx[i], p.getY() + dy[i], p));
 				}
 			}
 		}
 
-		//No Path
+		// No Path
 		return null;
 	}
 
-	public static void BreadthFirstSearch(int[][] maze, int x, int y, List<Integer> path) 
-	{
-		//Conduct BFS
-		Point p = FindPath(maze, x, y);
+	public static void pathfind(int[][] maze, int x, int y, List<Integer> path, List<Integer> searchArea) {
+		// Conduct BFS
+		Point p = FindPath(maze, x, y, searchArea);
 
-		if(p != null) 
-		{
-			//Back Track Parent Nodes
-			while(p.getParent() != null) 
-			{
-				//Add Parent to Path
-				path.add(p.x);
-				path.add(p.y);
+		if (p != null) {
+			// Back Track Parent Nodes
+			while (p.getParent() != null) {
 
-				//Next Parent
+				// Add Parent to Path
+				path.add(p.getX());
+				path.add(p.getY());
+
+				// Next Parent
 				p = p.getParent();
 			}
 		}
 
-		//Clear Queue
+		// Clear Queue
 		points.clear();
 	}
 }
--- Pathfinding/DFS.java
+++ Pathfinding/DFS.java
@@ -1,34 +1,34 @@
-package MazeProject;
+package Pathfinding;
 
 import java.util.List;
 
-public class DFS 
-{
-	public static int DepthFirstSearch(int[][] maze, int x, int y, List<Integer> path) 
-	{
-		//Destination Node Found
-		if(maze[y][x] == 9) 
-		{
-			//Add Square to Path
+public class DFS {
+	public static int pathfind(int[][] maze, int x, int y, List<Integer> path, List<Integer> searchArea) {
+		// Destination Node Found
+		if (maze[y][x] == 9) {
+
+			// Add Square to Path
 			path.add(x);
 			path.add(y);
-			
+
 			return 1;
 		}
 
-		if(maze[y][x] == 0) 
-		{
+		if (maze[y][x] == 0) {
 			maze[y][x] = 3;
 
-			//Check Adjacent Squares
+			// Add Square to Search Area
+			searchArea.add(x);
+			searchArea.add(y);
+
+			// Check Adjacent Squares
 			int[] dx = { 1, 0, -1, 0 };
 			int[] dy = { 0, 1, 0, -1 };
 
-			for(int i = 0; i < 4; i++) 
-			{
-				if(DepthFirstSearch(maze, x + dx[i], y + dy[i], path) == 1) 
-				{
-					//Add Square to Path
+			for (int i = 0; i < 4; i++) {
+				if (pathfind(maze, x + dx[i], y + dy[i], path, searchArea) == 1) {
+
+					// Add Square to Path
 					path.add(x);
 					path.add(y);
 
